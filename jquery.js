76e566bcc5f299c860f77df8c59e26debf19e22a
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-01-01 01:41:19 +0800

    add(jquery.js)

---
+++ jquery.js
@@ -0,0 +1,23 @@
+/*!
+
+          fn = function( elem, key, value ) {
+
+      i = 0,
+          markFunction(function( seed, matches ) {
+      temp = condense( matcherOut, postMap );
+    firingIndex,
+
+
+          i = options.length;
+        // when an event is called after a page has unloaded
+    // Support: Chrome 23+, Safari?
+        data = selector;
+
+        (elem.nodeType === 1 || elem.nodeType === 11) && !jQuery.isXMLDoc(elem) ) {
+  toggle: function( state ) {
+        style.removeAttribute( "filter" );
+    processData: true,
+
+        send: function( headers, complete ) {
+      });
+};
