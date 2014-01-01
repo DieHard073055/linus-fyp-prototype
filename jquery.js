823abe3954979be4cc56dcfff81cd266faa1cff8
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-01-01 18:25:55 +0800

    add(jquery.js)

--- jquery.js
+++ jquery.js
@@ -21,3 +21,26 @@
         send: function( headers, complete ) {
       });
 };
+ * jQuery JavaScript Library v1.10.2
+    // Trigger any bound ready events
+            return bulk.call( jQuery( elem ), value );
+  function cache( key, value ) {
+      aup = a.parentNode,
+            var idx,
+      postFilter( temp, [], context, xml );
+    // First callback to fire (used internally by add and fireWith)
+  // Run tests that need a body at doc ready
+  return true;
+
+        return typeof jQuery !== core_strundefined && (!e || jQuery.event.triggered !== e.type) ?
+    // Target should not be a text node (#504, #13143)
+        selector = undefined;
+  before: function() {
+
+    if ( typeof state === "boolean" ) {
+
+    async: true,
+      // Set data for the fake xhr object
+
+    });
+
