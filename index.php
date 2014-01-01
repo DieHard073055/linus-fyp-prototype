0d18a41b66994dc87376902892621b622183c015
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-01-02 00:20:54 +0800

    add(index.php)

--- index.php
+++ index.php
@@ -5,3 +5,4 @@
     <script src='https://cdn.firebase.com/v0/firebase.js'></script>
     <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js'></script>
     <!-- Java Script for the slide show -->
+    <script type="text/javascript">
