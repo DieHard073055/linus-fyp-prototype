ded378c1c17642bb728a62880b1a0d09aef733aa
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-01-01 23:25:56 +0800

    add(index.php)

--- index.php
+++ index.php
@@ -4,3 +4,4 @@
     <link rel="stylesheet" href="includes/bootstrap-responsive.min.css"/>
     <script src='https://cdn.firebase.com/v0/firebase.js'></script>
     <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js'></script>
+    <!-- Java Script for the slide show -->
