64366aeccbbd12b880a3b8a4145bedf8d2abe455
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-02-06 18:41:35 +0800

    add(order.php)

--- order.php
+++ order.php
@@ -130,3 +130,4 @@
 //           // $('<p/>').text(text).prepend($('<h4/>').text(name+': ')).prependTo($('#messagesDiv2'));
 //           // $('#messagesDiv2')[0].scrollTop = $('#messagesDiv2')[0].scrollHeight;
 
+//           $('<blockqoute/>').text(" ").prepend($('<p/>').text(text)).prepend($('<h4/>').text(name+'')).prependTo($('#messagesDiv2'));
