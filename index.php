bf9e533adc2f77728b2b2a646c7ad8a9b2a08c7f
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-02-13 21:33:17 +0800

    add(index.php)

--- index.php
+++ index.php
@@ -161,3 +161,4 @@
           <a href="http://www.yamu.lk/wp-content/uploads/2014/07/Mushroom_Biriyani_.jpg"><img src="http://www.yamu.lk/wp-content/uploads/2014/07/Mushroom_Biriyani_.jpg" alt="Slideshow Image 2" /></a>
           <p>Nasi Briyani</p>
       </div>
+      
