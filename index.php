a36b27a53871f2049f696383f999f771ede55f52
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-02-14 17:26:24 +0800

    add(index.php)

--- index.php
+++ index.php
@@ -164,3 +164,4 @@
       
       <div>
           <a href="http://upload.wikimedia.org/wikipedia/commons/d/d6/Laksa.JPG"><img src="http://upload.wikimedia.org/wikipedia/commons/d/d6/Laksa.JPG" alt="Slideshow Image 3" /></a>
+          <p>Sarawak laksa</p>
