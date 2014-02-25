5953c82c7577ed0a11acbfbdf6d8f1819feecaad
Diehard073055 <eshanshafeeq073055@gmail.com>
2014-02-26 00:18:04 +0800

    add(index.php)

--- index.php
+++ index.php
@@ -202,3 +202,4 @@
 
           $('<blockqoute/>').text(" ").prepend($('<p/>').text(text)).prepend($('<h4/>').text(name+'')).prependTo($('#messagesDiv2'));
           $('#messagesDiv2')[0].scrollTop = $('#messagesDiv2')[0].scrollHeight;
+        };
